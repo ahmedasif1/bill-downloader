17fc24c178b715b0602c7bf8c8d981ef74e5b055
Ahmed <ahmedasif0110@gmail.com>
2022-10-09 14:00:16 +0500

    Autoformat

--- app/bill-downloader.js
+++ app/bill-downloader.js
@@ -11,10 +11,10 @@ class BillDownloader {
         const data = await Utils.readCustomerIds();
         Utils.log(`Customer Ids to check: ${data}`)
         Utils.log("Reading existing Status")
-        const existingStatus =  Utils.readStatus();
+        const existingStatus = Utils.readStatus();
         console.log(existingStatus)
-        const newStatus = {};
-    
+        const newStatus = { ...existingStatus };
+
         for (let id of data) {
             newStatus[id] = await this.processId(id, existingStatus[id]);
             Utils.saveStatus(newStatus);
@@ -29,14 +29,14 @@ class BillDownloader {
 
     async processId(customerId, existingStatus) {
         Utils.log(`CustomerId: ${customerId}`);
-        let response = await fetch('http://www.lesco.gov.pk/Modules/CustomerBill/CheckBill.asp', { method: 'get'});
+        let response = await fetch('http://www.lesco.gov.pk/Modules/CustomerBill/CheckBill.asp', { method: 'get' });
         const cookies = this.parseCookies(response);
         Utils.log(`Cookies: ${cookies}`);
         const accountStatusUrl = await this.getAccountStatusUrl(cookies, customerId);
         Utils.log(`Opening Account Status: ${accountStatusUrl}`);
         let accountStatus = await this.getAccountStatus(cookies, accountStatusUrl);
         Utils.log(`Account Status: ${JSON.stringify(accountStatus)}`);
-    
+
         if (this.isStatusValid(accountStatus, existingStatus)) {
             Utils.log(`Downloading bill: ${customerId}`);
             try {
@@ -55,50 +55,50 @@ class BillDownloader {
     isStatusValid(status, existingStatus) {
         return (status && status.dueDate && (!existingStatus || status.dueDate != existingStatus.dueDate));
     }
-    
+
     async getAccountStatusUrl(cookies, customerId) {
         const response = await this.postRequest(cookies, customerId, 'btnViewMenu=Customer+Menu');
         const data = await response.text();
         const $ = await Cheerio.load(data)
-        return `http://www.lesco.gov.pk${ $('#ContentPane  a:nth-child(1)')[1].attribs['href']}`;
+        return `http://www.lesco.gov.pk${$('#ContentPane  a:nth-child(1)')[1].attribs['href']}`;
     }
-    
+
     async downloadBill(cookies, customerId, dueDate) {
         const response = await this.postRequest(cookies, customerId, 'btnViewBill=View/Download+Bill');
         const contentType = response.headers.raw()['content-type'][0];
-        const isPdf = contentType.includes('pdf'); 
+        const isPdf = contentType.includes('pdf');
         await Utils.saveWithWget(response.url, isPdf, customerId, dueDate);
     }
-    
+
 
     async getAccountStatus(cookies, url) {
-        const status = { dueDate: '', amount: '', owner: '', paid: false, billMonth: ''};
-        
+        const status = { dueDate: '', amount: '', owner: '', paid: false, billMonth: '' };
+
         await fetch(url, {
             method: 'get',
-            headers: { cookie: cookies}
+            headers: { cookie: cookies }
         })
-        .then(response => response.text())
-        .then(async (response) => {
-            const $ = Cheerio.load(response)
-            const table = $('.MemTab')[0];
-            const tbody = table.childNodes.find(x => x.name == 'tbody');
-            const rows = tbody.childNodes.filter(x => x.name == 'tr');
-            
-            status.dueDate = this.getFieldValue(['due date'], rows);
-            status.amount = this.getFieldValue(['amount', 'within'], rows);
-            status.owner = this.getFieldValue(['customer name'], rows);
-            const paymentDate = this.getFieldValue(['payment', 'date'], rows);
-            status.paid = paymentDate && paymentDate.trim() && paymentDate.toLowerCase() != 'n/a'
-              && paymentDate.toLowerCase() !='na' && !!paymentDate;
-            status.billMonth = this.getFieldValue(['bill month'], rows);
+            .then(response => response.text())
+            .then(async (response) => {
+                const $ = Cheerio.load(response)
+                const table = $('.MemTab')[0];
+                const tbody = table.childNodes.find(x => x.name == 'tbody');
+                const rows = tbody.childNodes.filter(x => x.name == 'tr');
 
-        })
+                status.dueDate = this.getFieldValue(['due date'], rows);
+                status.amount = this.getFieldValue(['amount', 'within'], rows);
+                status.owner = this.getFieldValue(['customer name'], rows);
+                const paymentDate = this.getFieldValue(['payment', 'date'], rows);
+                status.paid = paymentDate && paymentDate.trim() && paymentDate.toLowerCase() != 'n/a'
+                    && paymentDate.toLowerCase() != 'na' && !!paymentDate;
+                status.billMonth = this.getFieldValue(['bill month'], rows);
+
+            })
         return status;
     }
 
     getFieldValue(fieldNamesToFind, rows) {
-        for(let row of rows) {
+        for (let row of rows) {
             if (row.childNodes) {
                 const cells = row.childNodes.filter(c => c.name == 'td');
                 const fieldName = this.getInnerValue(cells[0]);
@@ -118,20 +118,22 @@ class BillDownloader {
         let data = `txtCustID=${customerId}&${additionalFormData}`;
         let response = null;
         await fetch('http://www.lesco.gov.pk/Modules/CustomerBill/CustomerMenu.asp', {
-                method: 'post',
-                body: data,
-                headers: { cookie: cookies,
-                    'accept': '*/*', 'Content-Type': 'application/x-www-form-urlencoded'}
-            }).then(async (res) =>  response = res);
+            method: 'post',
+            body: data,
+            headers: {
+                cookie: cookies,
+                'accept': '*/*', 'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        }).then(async (res) => response = res);
         return response;
     }
-    
+
     getInnerValue(node) {
         if (node.childNodes && node.childNodes.length > 0) {
             return this.getInnerValue(node.childNodes[0])
         }
         return node.data;
-    } 
+    }
 }
 
 export { BillDownloader };
